daec5bb8dc9e0b2a078acceecc03196bfd521393
uthumWijenayake <uthumwijenayake@gmail.com>
2024-10-15 21:15:11 +0530

    create program page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero'; 
+import Program from './Program';
+import Title from './Title';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className='container'>
+          <Title subtitle='Our Program' title= 'What We Offer'/>
+          <Program/>
+       
+      </div>
+      
     </div>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from './assets/logo.png';
 
 const Navbar = () => {
+    
+     const[sticky,setSticky]= useState(false);
+     useEffect (()=>{
+          window.addEventListener('scroll',()=>{
+            window.scrollY > 50 ? setSticky(true):setSticky(false);
+          })
+     },[]);
   return (
-    <nav className="navbar">
+    <nav className={`navbar $ {sticky? 'dark-nav' :''}`}>
       <img src={logo} alt="logo" className="logo" />
       <ul>
         <li>Home</li>
